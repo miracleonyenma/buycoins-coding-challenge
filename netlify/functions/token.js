d0f5a92b08683a28757c9af079cff70f772bc325
miracleonyenma <miracleficient@gmail.com>
2021-05-30 03:35:30 +0100

    update: change token in process

--- netlify/functions/token.js
+++ netlify/functions/token.js
@@ -1,6 +1,6 @@
 exports.handler = async function (event, context) {
     return {
         statusCode: 200,
-        body: JSON.stringify({msg: process.env.DEPLOY_URL})
+        body: JSON.stringify({msg: process.env.TOKEN})
     }
 }
